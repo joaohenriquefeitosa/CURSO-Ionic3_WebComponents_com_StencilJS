882689784deabf4b2a8f40c0f4e45966a481ffbf
John Feitosa <joaohenriquefsf@gmail.com>
2019-05-09 00:03:52 -0300

    first commit

---
+++ src/pages/angular/binding/binding.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the BindingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-binding',
+  templateUrl: 'binding.html',
+})
+export class BindingPage {
+
+  public valor: number = 1;
+  public valor2: number = 2;
+
+  public nome: string = 'Gabriel';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BindingPage');
+  }
+
+  retornaValor(){
+    return this.valor2;
+  }
+}
